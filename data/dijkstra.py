a20ed95a4e51f7376b16ab9301d20b4a9bb83795
arttiwa <atiwagonr1@gmail.com>
2023-08-30 20:06:09 +0700

    DDDDD))))

---
+++ data/dijkstra.py
@@ -0,0 +1,31 @@
+from graph import Graph
+
+def dijkstra(graph, src, dest):
+    shortest_paths = {src: (None, 0)}
+    visited = set()
+
+    while src != dest:
+        visited.add(src)
+        destinations = graph.edges[src]
+        weight_to_src_node = shortest_paths[src][1]
+
+        for next_node in destinations:
+            weight = graph.weights[(src, next_node)] + weight_to_src_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (src, weight)
+            else:
+                src_shortest_weight = shortest_paths[next_node][1]
+                if src_shortest_weight > weight:
+                    shortest_paths[next_node] = (src, weight)
+
+        candidates = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not candidates:
+            break
+        src = min(candidates, key=lambda k: candidates[k][1])
+
+    path = []
+    while dest is not None:
+        path.insert(0, dest)
+        dest = shortest_paths[dest][0]
+
+    return path
---
+++ data/process.php
@@ -0,0 +1,46 @@
+<?php
+// Include Graph class
+require_once 'graph.php';
+
+// Create the graph object
+
+//$graph_data  = array("A","B","C","D","E","F","G","EleF11_f4m","StairF11_f4c","F11_401",
+//    "F11_402","F11_414","F11_415","F11_419","F11_420","F11_421","F11_422","F11_423"
+//);
+
+$graph_data  = array(
+    'EleF11_f4m' => array('F11_401' => 6,'F11_402' => 6,'F11_414' => 10,'F11_415' => 9,'F11_419' => 5,'F11_420' => 4,'F11_421' => 4,'F11_422' => 5,'F11_423' => 6),
+    'StairF11_f4c' => array('F11_401' => 4,'F11_402' => 4,'F11_414' => 11,'F11_415' => 10,'F11_419' => 12,'F11_420' => 12,'F11_421' => 13,'F11_422' => 14,'F11_423' => 15),
+    'F11_401' => array('StairF11_f4c' => 4,'EleF11_f4m' => 6,'F11_402' => 1,'F11_414' => 8,'F11_415' => 9,'F11_419' => 9,'F11_420' => 8,'F11_421' => 8,'F11_422' => 9,'F11_423' => 13),
+    'F11_402' => array('StairF11_f4c' => 4,'EleF11_f4m' => 6,'F11_401' => 1,'F11_414' => 8,'F11_415' => 9,'F11_419' => 9,'F11_420' => 8,'F11_421' => 8,'F11_422' => 9,'F11_423' => 13),
+    'F11_414' => array('StairF11_f4c' => 11,'EleF11_f4m' => 10,'F11_401' => 8,'F11_402' => 8,'F11_415' => 1,'F11_419' => 5,'F11_420' => 6,'F11_421' => 5,'F11_422' => 8,'F11_423' => 9),
+    'F11_415' => array('StairF11_f4c' => 10,'EleF11_f4m' => 9,'F11_401' => 9,'F11_402' => 9,'F11_414' => 1,'F11_419' => 5,'F11_420' => 6,'F11_421' => 5,'F11_422' => 8,'F11_423' => 9),
+    'F11_419' => array('StairF11_f4c' => 12,'EleF11_f4m' => 5,'F11_401' => 9,'F11_402' => 9,'F11_414' => 5,'F11_415' => 5,'F11_420' => 1,'F11_421' => 3,'F11_422' => 5,'F11_423' => 6),
+    'F11_420' => array('StairF11_f4c' => 12,'EleF11_f4m' => 4,'F11_401' => 8,'F11_402' => 8,'F11_414' => 6,'F11_415' => 6,'F11_419' => 1,'F11_421' => 2,'F11_422' => 4,'F11_423' => 5),
+    'F11_421' => array('StairF11_f4c' => 13,'EleF11_f4m' => 4,'F11_401' => 8,'F11_402' => 8,'F11_414' => 5,'F11_415' => 5,'F11_419' => 3,'F11_420' => 2,'F11_422' => 1,'F11_423' => 2),
+    'F11_422' => array('StairF11_f4c' => 14,'EleF11_f4m' => 5,'F11_401' => 9,'F11_402' => 9,'F11_414' => 8,'F11_415' => 8,'F11_419' => 5,'F11_420' => 4,'F11_421' => 1,'F11_423' => 4),
+    'F11_423' => array('StairF11_f4c' => 15,'EleF11_f4m' => 6,'F11_401' => 10,'F11_402' => 10,'F11_414' => 9,'F11_415' => 9,'F11_419' => 6,'F11_420' => 5,'F11_421' => 2,'F11_422' => 1),
+    'A' => array('B' => 1,'C' => 4),
+    'B' => array('A' => 1,'C' => 2,'D' => 5),
+    'C' => array('A' => 4,'B' => 2,'D' => 1),
+    'D' => array('B' => 5,'C' => 1)
+);
+
+
+$graph_obj = new Graph($graph);
+
+$graph_obj = new Graph($graph_data);
+
+// Check if the form has been submitted
+if (isset($_POST['submit'])) {
+    // Get the selected Source and Destination nodes from the form
+    $source = $_POST['source'];
+    $destination = $_POST['destination'];
+
+    // Call the dijkstra function
+    $shortest_path = $graph_obj->dijkstra($source, $destination);
+
+    // Display the result
+    echo "Shortest path from $source to $destination: " . implode(' -> ', $shortest_path);
+}
+?>
