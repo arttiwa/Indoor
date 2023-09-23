47194957502ba1dce2a6d55638e951c88057e2b8
arttiwa <atiwagonr1@gmail.com>
2023-09-23 17:32:05 +0700

    now can find start and end but can't find pic between the way to go .it can find if use 1 valiable  = / can't use =  connect w/  can't find pic from file - close #13

--- test/capture.py
+++ test/capture.py
@@ -1,13 +1,31 @@
 import cv2
+#import pytesseract
 
+# เริ่มกล้อง
 cap = cv2.VideoCapture(0)
 
+# ตั้งค่าภาษาให้ Tesseract ตรวจจับ
+#custom_config = r'--oem 3 --psm 6 -l eng'
+
 while True:
+    # อ่านภาพจากกล้อง
     ret, frame = cap.read()
 
+    # แปลงภาพเป็นข้อความด้วย Tesseract
+    #custom_config = r'--oem 3 --psm 6 --lang eng --whitelist ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    #text = pytesseract.image_to_string(frame, config=custom_config)
+
+
+    # แสดงผลลัพธ์บนหน้าจอ
+    #cv2.imshow('Text Detection', frame)
+    #print(text)
     cv2.imshow('Camera', frame)
+   
+
+    # กด 'q' เพื่อออกจากโปรแกรม
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# ปิดกล้องและหน้าจอ
 cap.release()
 cv2.destroyAllWindows()
