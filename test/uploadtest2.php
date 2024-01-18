2f80d14c7b6ea56fb59566b0f18654a5a8f57d97
arttiwa <atiwagonr1@gmail.com>
2024-01-19 00:31:26 +0700

    UPGRADE* can copy and rename

--- test/uploadtest2.php
+++ test/uploadtest2.php
@@ -3,29 +3,75 @@
         $targetDir = "uploads/"; // specify the directory where you want to store the uploaded files
 
         // Function to handle file upload and return an error message if unsuccessful
-        function handleFileUpload($fileInputName, $targetDir, $pointName, $fileNameAll, $fileTag)
+        function handleFileUpload($fileInputName, $targetDir, $pointName, $fileNameAll)
         {
+            $copyTags = [];
+            $pointNames = [
+                $_POST["fileName1"],
+                $_POST["fileName2"],
+                $_POST["fileName3"],
+                $_POST["fileName4"]
+            ];
             $oldFileName = $_FILES[$fileInputName]["name"];
             $extension = pathinfo($oldFileName, PATHINFO_EXTENSION);
             $fileNameMid = $_POST["fileNameMid"];
             $setFileNameAll = $_POST[$fileNameAll];
 
-            // Modify the copy file name to include the specified tag
-            $copyFileName = $fileNameMid . "_" . $setFileNameAll . "_" . $fileTag . "." . $extension;
-
             $newFileName = $fileNameMid . "_" . $setFileNameAll . "." . $extension;
             $file = $targetDir . $newFileName;
-            $copyFile = $targetDir . $copyFileName;
             $uploadOk = 1;
 
             // Move the file to the specified directory
             if (move_uploaded_file($_FILES[$fileInputName]["tmp_name"], $file)) {
-                // Copy the file with a new name
-                if (!copy($file, $copyFile)) {
-                    return "Error copying file.";
+
+                // assign data to  
+                if ($fileNameAll == "fileName1") {
+                    $copyTags = [
+                        $pointNames[0] . "_T",
+                        $pointNames[1] . "_R",
+                        $pointNames[2] . "_L",
+                        $pointNames[3] . "_B"
+                    ];
+                } elseif ($fileNameAll == "fileName2") {
+                    $copyTags = [
+                        $pointNames[0] . "_L",
+                        $pointNames[1] . "_T",
+                        $pointNames[2] . "_B",
+                        $pointNames[3] . "_R"
+                    ];
+                } elseif ($fileNameAll == "fileName3") {
+                    $copyTags = [
+                        $pointNames[0] . "_R",
+                        $pointNames[1] . "_B",
+                        $pointNames[2] . "_T",
+                        $pointNames[3] . "_L"
+                    ];
+                } elseif ($fileNameAll == "fileName4") {
+                    $copyTags = [
+                        $pointNames[0] . "_B",
+                        $pointNames[1] . "_L",
+                        $pointNames[2] . "_R",
+                        $pointNames[3] . "_T"
+                    ];
+                } else {
+                    return "Error sssssss file.";
                 }
 
-                return "File uploaded successfully. Old name: " . $oldFileName . ", New name: " . $newFileName . "<br>Copied as File 1: " . $copyFileName;
+                $copyMessages = [];
+
+                foreach ($copyTags as $tag) {
+
+                    $copyFileName = $fileNameMid . "_" . $tag . "." . $extension;
+                    $copyFile = $targetDir . $copyFileName;
+
+                    if (copy($file, $copyFile)) {
+                        $copyMessages[] = "Copied as File 1: $copyFileName";
+                    } else {
+                        $copyMessages[] = "Error copying file for tag $tag.";
+                    }
+                }
+                
+                return " File uploaded successfully. <br>Old name: $oldFileName,<br> New name: $newFileName<br>" . implode("<br>", $copyMessages);
             } else {
                 return "Error uploading file.";
             }
@@ -33,54 +79,49 @@
 
         // Process File 1
         $pointName1 = $_POST["fileName1"];
-        $message1 = implode("<br>Copied as File 1: ", array_map(function ($tag) use ($pointName1, $targetDir) {
-            return handleFileUpload("fileUpload1", $targetDir, $pointName1, "fileName1", $tag);
-        }, ['T', 'R', 'L', 'B']));
+        $message1 = handleFileUpload("fileUpload1", $targetDir, $pointName1, "fileName1");
 
         // Process File 2
         $pointName2 = $_POST["fileName2"];
-        $message2 = implode("<br>Copied as File 2: ", array_map(function ($tag) use ($pointName2, $targetDir) {
-            return handleFileUpload("fileUpload2", $targetDir, $pointName2, "fileName2", $tag);
-        }, ['L', 'T', 'B', 'R']));
+        $message2 = handleFileUpload("fileUpload2", $targetDir, $pointName2, "fileName2");
 
         // Process File 3
         $pointName3 = $_POST["fileName3"];
-        $message3 = implode("<br>Copied as File 3: ", array_map(function ($tag) use ($pointName3, $targetDir) {
-            return handleFileUpload("fileUpload3", $targetDir, $pointName3, "fileName3", $tag);
-        }, ['R', 'B', 'T', 'L']));
+        $message3 = handleFileUpload("fileUpload3", $targetDir, $pointName3, "fileName3");
 
         // Process File 4
         $pointName4 = $_POST["fileName4"];
-        $message4 = implode("<br>Copied as File 4: ", array_map(function ($tag) use ($pointName4, $targetDir) {
-            return handleFileUpload("fileUpload4", $targetDir, $pointName4, "fileName4", $tag);
-        }, ['B', 'L', 'R', 'T']));
+        $message4 = handleFileUpload("fileUpload4", $targetDir, $pointName4, "fileName4");
 
-        // Display uploaded images only if they exist
-        if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload1"]["name"])) {
-            echo "File 1:<br>";
-            echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload1"]["name"] . "' alt='File 1' width='300' height='300'><br>";
-        }
+        // Output the messages
+        echo "<br><br>File 1: " . $message1 . "<br>";
+        echo "<br><br>File 2: " . $message2 . "<br>";
+        echo "<br><br>File 3: " . $message3 . "<br>";
+        echo "<br><br>File 4: " . $message4 . "<br>";
 
-        if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload2"]["name"])) {
-            echo "File 2:<br>";
-            echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload2"]["name"] . "' alt='File 2' width='300' height='300'><br>";
-        }
 
-        if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload3"]["name"])) {
-            echo "File 3:<br>";
-            echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload3"]["name"] . "' alt='File 3' width='300' height='300'><br>";
-        }
+        // // Display uploaded images only if they exist
+        // if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload1"]["name"])) {
+        //     echo "File 1:<br>";
+        //     echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload1"]["name"] . "' alt='File 1' width='300' height='300'><br>";
+        // }
 
-        if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload4"]["name"])) {
-            echo "File 4:<br>";
-            echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload4"]["name"] . "' alt='File 4' width='300' height='300'><br>";
-        }
 
-        // You can add more processing logic or database operations here if needed
+        // if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload2"]["name"])) {
+        //     echo "File 2:<br>";
+        //     echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload2"]["name"] . "' alt='File 2' width='300' height='300'><br>";
+        // }
 
-        echo "File 1: " . $message1 . "<br>";
-        echo "File 2: " . $message2 . "<br>";
-        echo "File 3: " . $message3 . "<br>";
-        echo "File 4: " . $message4 . "<br>";
+        // if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload3"]["name"])) {
+        //     echo "File 3:<br>";
+        //     echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload3"]["name"] . "' alt='File 3' width='300' height='300'><br>";
+        // }
+
+        // if (file_exists($targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload4"]["name"])) {
+        //     echo "File 4:<br>";
+        //     echo "<img src='" . $targetDir . $_POST["fileNameMid"] . "_" . $_FILES["fileUpload4"]["name"] . "' alt='File 4' width='300' height='300'><br>";
+        // }
+
+
     }
 ?>
